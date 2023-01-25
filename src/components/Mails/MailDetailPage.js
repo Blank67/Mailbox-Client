c94d081043c4db4fbb828eb97cf04c8afbbdecbe
Blank67 <hmnshshrm67@gmail.com>
2023-01-25 10:49:26 +0530

    count and mail detail page done

--- src/components/Mails/MailDetailPage.js
+++ src/components/Mails/MailDetailPage.js
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { Card, Col, Container, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { postAllData } from "../../store/http-request/mail-http";
@@ -10,16 +11,39 @@ const MailDetailPage = (props) => {
 
     const mail = mailSlice.mails.filter((itm) => itm.id === params.key);
 
+    // const deleteMail = () => {
+    //     debugger;
+    //     console.log("Button Clicked!");
+    //     console.log(mail);
+    //     dispatch(mailActions.deleteMail({ mail: mail, for: 'INBOX' }));
+    // }
     useEffect(() => {
         dispatch((postAllData(mailSlice.mails)));
     }, [dispatch, mailSlice]);
 
     return (
-        <div>
-            <div>{mail[0].sEmail}</div>
-            <div>{mail[0].subject}</div>
-            <div>{mail[0].body}</div>
-        </div>
+        <Container style={{ marginTop: "60px" }}>
+            <Card>
+                <Card.Header>
+                    <Row>
+                        <Col>
+                            <span>From: {mail[0].sEmail}</span>
+                        </Col>
+                        {/* <Col>
+                            <Button variant="danger" className="float-end" onClick={deleteMail}>
+                                <Delete />
+                            </Button>
+                        </Col> */}
+                    </Row>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Title>Subject: {mail[0].subject}</Card.Title>
+                    <Card.Text>
+                        {mail[0].body}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        </Container>
     );
 }
 
--- src/components/SideBar/SidebarOptions.js
+++ src/components/SideBar/SidebarOptions.js
@@ -1,10 +1,15 @@
+import { useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import "../../css/SidebarOptions.css";
 
 const SidebarOptions = (props) => {
+    const mailSlice = useSelector((state) => state.mail);
+
+    const count = props.name === "Inbox" ? mailSlice.inbox.length : mailSlice.outbox.length;
     return (
         <div className={`option__item my-1`}>
-            <NavLink className="nav-link" to={props.path}>{props.name}</NavLink>
+            <NavLink className="nav-link px-3" to={props.path}>{props.name}</NavLink>
+            <span>{count}</span>
         </div>
     );
 }
