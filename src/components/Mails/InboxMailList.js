f71e8e031585eea80318b1c3d4b4a86c42caa186
Blank67 <hmnshshrm67@gmail.com>
2023-02-16 16:59:42 +0530

    spinner added

--- src/components/Mails/InboxMailList.js
+++ src/components/Mails/InboxMailList.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { mailActions } from "../../store/mails-slice/mails-slice";
 import InboxMailItem from './InboxMailItem';
+import Spinner from 'react-bootstrap/Spinner';
 
 const InboxMailList = () => {
     const mailSlice = useSelector(state => state.mail);
@@ -14,7 +15,7 @@ const InboxMailList = () => {
 
     return (
         <ul style={{ listStyle: "none" }} className="justify-content-start mt-4">
-            {mailSlice.inbox.length > 0 ? itemList : mailSlice.mails.length === 0 ? <h2 className="text-center" style={{ marginTop: '250px', marginRight: '150px' }}>Loading....</h2> : <h2 className="text-center" style={{ marginTop: '250px', marginRight: '150px' }}>Sorry, you don't have any mails.</h2>}
+            {mailSlice.inbox.length > 0 ? itemList : mailSlice.mails.length === 0 ? <span className="d-flex justify-content-center"><Spinner animation="grow" variant="secondary" style={{ marginTop: '250px', marginRight: '150px' }} /></span> : <h2 className="text-center" style={{ marginTop: '250px', marginRight: '150px' }}>Sorry, you don't have any mails.</h2>}
         </ul>
     );
 }
--- src/components/Mails/OutboxMailList.js
+++ src/components/Mails/OutboxMailList.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import OutBoxMailItem from "./OutboxMailItem";
 import { mailActions } from "../../store/mails-slice/mails-slice";
+import { Spinner } from "react-bootstrap";
 
 const OutboxMailList = (props) => {
     const mailSlice = useSelector(state => state.mail);
@@ -13,7 +14,7 @@ const OutboxMailList = (props) => {
 
     return (
         <ul style={{ listStyle: "none" }} className="justify-content-start mt-4">
-            {mailSlice.outbox.length > 0 ? itemList : mailSlice.mails.length === 0 ? <h2 className="text-center" style={{ marginTop: '250px', marginRight: '150px' }}>Loading....</h2> : <h2 className="text-center" style={{ marginTop: '250px', marginRight: '150px' }}>Your outbox is empty.</h2>}
+            {mailSlice.outbox.length > 0 ? itemList : mailSlice.mails.length === 0 ? <span className="d-flex justify-content-center"><Spinner animation="grow" variant="secondary" style={{ marginTop: '250px', marginRight: '150px' }} /></span> : <h2 className="text-center" style={{ marginTop: '250px', marginRight: '150px' }}>Your outbox is empty.</h2>}
         </ul>
     );
 }
